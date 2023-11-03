d63739d42ead8ee5313c6916f327c501aed74eba
yangwonjoon <diddnjswns20@gmail.com>
2023-11-03 20:58:59 +0900

    [feat] 풀캘린더 구글캘린더 연동 테스트

--- front/src/pages/Main/customerMain.js
+++ front/src/pages/Main/customerMain.js
@@ -1,5 +1,7 @@
 import { useNavigate } from "react-router-dom";
 
+import googleCalendarPlugin from '@fullcalendar/google-calendar';
+
 import listPlugin from '@fullcalendar/list';
 import FullCalendar from '@fullcalendar/react'
 import dayGridPlugin from '@fullcalendar/daygrid'
@@ -9,6 +11,7 @@ import interactionPlugin from "@fullcalendar/interaction"
 function CustomerMain() {
 
     const navigate = useNavigate();
+    const api_key = "AIzaSyD-zVEehdfIqaeN9s944ahImpAvzOpG2do"
 
     const dateClick = (e) => {
         //navigate('/login')
@@ -20,9 +23,10 @@ function CustomerMain() {
             <div style={{ width: "1000px" }}>
                 <FullCalendar
 
-                    plugins={[dayGridPlugin, interactionPlugin, timeGridPlugin, listPlugin]} //플러그인 설치
+                    plugins={[dayGridPlugin, interactionPlugin, timeGridPlugin, listPlugin, googleCalendarPlugin]} //플러그인 설치
                     initialView="dayGridMonth" //처음 보여주는 뷰
                     weekends={true} //주말 포함 여부d
+                    googleCalendarApiKey={api_key}
                     height={'600px'}
                     headerToolbar={
                         {
@@ -31,9 +35,9 @@ function CustomerMain() {
                             end: 'listDay,listWeek,listMonth prev,next',
                         }
                     }
-                    events={[
-                        { id: 1, title: 'startDay', date: '2023-11-01' }
-                    ]}
+                    events={{
+                        googleCalendarId: 'diddnjswns20@gmail.com',
+                    }}
                     dateClick={dateClick}
                     views={{
                         listDay: { buttonText: 'Day' },
@@ -43,6 +47,22 @@ function CustomerMain() {
 
                 />
             </div>
+            {/* <div className="cal-container">
+                <FullCalendar
+                    plugins={[dayGridPlugin, googleCalendarPlugin]}
+                    initialView="dayGridMonth"
+                    googleCalendarApiKey={api_key}
+                    events={{
+                        googleCalendarId: 'diddnjswns20@gmail.com',
+                    }}
+                    eventDisplay={'block'}
+                    eventTextColor={'#FFF'}
+                    eventColor={'#F2921D'}
+                    height={'660px'}
+                    Toolbar;
+
+                />
+            </div> */}
         </>
     )
 }
